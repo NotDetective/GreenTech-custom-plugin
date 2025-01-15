422065b56e92271f460ee0739d6a57c92553bee0
Micha <micha-elmans@outlook.com>
2025-01-15 11:35:44 +0100

    Initial commit

---
+++ GreenTech_custom_plugin.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: GreenTech Custom Plugin
+Description: A brief description of the plugin.
+Version: 1.0.0
+Author: Micha Elmans, Bo Leenders
+*/
---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
+// Cleanup tasks, e.g., delete_option( 'my_plugin_option' );
+
